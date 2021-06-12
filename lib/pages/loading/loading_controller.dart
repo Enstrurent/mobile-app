26ace0c0a553beb8d6330f531a7d450e2d88a51c
Fatih <fatihy@yahoo.com>
2021-06-12 23:20:16 +0300

    Code improvements

--- lib/pages/loading/loading_controller.dart
+++ lib/pages/loading/loading_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:enstrurent/services/auth.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:get/get.dart';
@@ -6,25 +8,14 @@ class LoadingController extends GetxController {
   Auth _auth = Auth();
 
   callNavigate() => SchedulerBinding.instance.addPostFrameCallback((_) {
-    _navigateToPage();
-  });
+        _navigateToPage();
+      });
 
   _navigateToPage() async {
-    var role = await _auth.checkCurrentUser();
-    switch(role) {
-      case Roles.CLIENT:
-        Get.offNamed("/client");
-        break;
-      case Roles.RENTER:
-        Get.offNamed("/renter");
-        break;
-      case Roles.GUEST:
-        Get.offNamed("/guest");
-        break;
-      default:
-        Get.offNamed("/guest");
-    }
-
+    var role = await _auth.validateCurrentUser();
+    log(role.toString(), name: "Role");
+    roleString.forEach((key, value) {
+      if (role == key) Get.offNamed("/$value");
+    });
   }
-
 }
