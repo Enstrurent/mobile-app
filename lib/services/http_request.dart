14105817d31fd1405eeaadd4ad3711fa76d1c9b3
Fatih <fatihy@yahoo.com>
2021-07-14 22:01:23 +0300

    sensitive info deleted.

--- lib/services/http_request.dart
+++ lib/services/http_request.dart
@@ -8,7 +8,7 @@ enum Request { POST, GET, DELETE, PUT }
 
 class HttpRequest extends GetxController {
   SecureStore _store = Get.find();
-  static String host = "***REMOVED***";
+  static String host = "";
   var dioObj = dio.Dio(); //  For sending multipart files with form-data.
   String token = "";
   Map<String, String> headers = {
